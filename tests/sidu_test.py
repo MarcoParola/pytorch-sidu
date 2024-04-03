b418c0a81b7c895b92022b8aa3f73f6f2e6cd9f0
MarcoParola <marcoparola96@gmail.com>
2024-04-03 12:05:39 +0200

    add parametrized pytest tests for testing multiple models

--- tests/sidu_test.py
+++ tests/sidu_test.py
@@ -1,16 +1,28 @@
 import torch
 import torchvision
 import pytorch_sidu as sidu
+import pytest
 
 
-def test_sidu():
-    model = torchvision.models.resnet18(weights='ResNet18_Weights.IMAGENET1K_V1')
+@pytest.mark.parametrize("model_name, layer_name", [
+    ('ResNet18_Weights.IMAGENET1K_V1', 'layer4.1.conv2'),
+    ('ResNet34_Weights.IMAGENET1K_V1', 'layer4.2.conv2'),
+    ('ViT_B_16_Weights.IMAGENET1K_V1', 'conv_proj'),
+    ('ViT_B_32_Weights.IMAGENET1K_V1', 'conv_proj'),
+])
+def test_sidu(layer_name, model_name):
+
     batch_size = 1
-    w = 256
-    h = 256
-    layer_name = 'layer4.1.conv2'
+    w = 224
+    h = 224
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    name, version = model_name.split(".")
+    name = name.split("_Weights")[0].lower()
+    model = getattr(torchvision.models, name)(weights=model_name)
+    model.eval()
 
     image = torch.rand(batch_size, 3, w, h)
-    saliency_map = sidu.sidu(model, layer_name, image, device=torch.device('cuda'))
+    saliency_map = sidu.sidu(model, layer_name, image, device=device)
     assert saliency_map.shape == (batch_size, w, h)
 
